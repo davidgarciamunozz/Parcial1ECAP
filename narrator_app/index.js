d96474692e637c0fba120e35f1b6760cf1020589
David García Muñoz <148887266+davidgarciamunozz@users.noreply.github.com>
2025-03-28 08:55:54 -0500

    narrator enhanced

--- narrator_app/index.js
+++ narrator_app/index.js
@@ -1,5 +1,6 @@
 const dayButton = document.getElementById('dayBtn')
 const nightButton = document.getElementById('nightBtn')
+const gameStatus = document.getElementById('game-status')
 
 async function makeDay() {
     const res = await fetch('/notificar-dia', {
@@ -11,9 +12,14 @@ async function makeDay() {
     })
 
     const data = await res.json()
+    
+    if (data.status === 'success') {
+        gameStatus.textContent = 'Estado: Es de día - Los jugadores han sido notificados'
+        gameStatus.style.backgroundColor = '#8be9fd'
+        gameStatus.style.color = '#282a36'
+    }
 
     console.log(data)
-    
 }
 
 async function makeNight(){
@@ -25,11 +31,15 @@ async function makeNight(){
         body: JSON.stringify({message: 'evento noche desde el narrador'})
     })
     const data = await res.json()
+    
+    if (data.status === 'success') {
+        gameStatus.textContent = 'Estado: Es de noche - Los jugadores han sido notificados'
+        gameStatus.style.backgroundColor = '#6272a4'
+        gameStatus.style.color = '#f8f8f2'
+    }
 
     console.log(data)
 }
 
 dayButton.addEventListener('click', makeDay)
 nightButton.addEventListener('click', makeNight)
-
-
